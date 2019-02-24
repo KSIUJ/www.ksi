54f61ee9317b8e2eba77c7416009ab3a50962760
Adam Pardyl <adam@pardyl.com>
2019-02-24 19:44:38 +0100

    Remove unused scrollex.js

--- jekyll/js/main.js
+++ jekyll/js/main.js
@@ -27,20 +27,6 @@
 			}, 100);
 		});
 
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
-
-			$window.on('resize', function() { $window.trigger('scroll'); });
-
-			$banner.scrollex({
-				bottom:		$header.outerHeight(),
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
-
-		}
 
 	// Menu.
 		var $menu = $('#menu');
